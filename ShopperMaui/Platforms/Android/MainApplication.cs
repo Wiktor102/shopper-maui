38b430093eddaa53116af6958bbe87d237542d96
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-11-29 19:04:02 +0100

    feat: init project

---
+++ ShopperMaui/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace ShopperMaui
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
